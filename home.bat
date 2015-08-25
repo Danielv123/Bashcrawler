458f61e57a9c42dea75541b6a036367bf5a20781
Danielv123 <tranen.bananen@gmail.com>
2015-08-25 17:34:53 +0200

    !IMPORTANT everything fixed this time for realz. Maybe.
    Added a new function for creating savegames as the previous would have
    transformed into a mess (and it didn't work) updated stuff to reflect
    the new menubuttons and added a "quit" function. Important that. The
    levels in that folder is now moved in a way that actually works. That
    should be an advantage, right?

--- home.bat
+++ home.bat
@@ -2,21 +2,39 @@
 @echo off
 @title The adventures of Batch
 color 0a
-if not exist savegame.cmd echo set x=1 > savegame.cmd
-if not exist savegame.cmd echo set y=1 >> savegame.cmd 
+
 :main
 type menu.txt
 
 set /p selection=">_ "
-if "%selection%" == "start" call start.bat
-if "%selection%" == "start game" call start.bat
+
+if "%selection%" == "new" goto newgame
+if "%selection%" == "new game" goto newgame
+
 if "%selection%" == "load" call savegame.cmd
 if "%selection%" == "load game" call savegame.cmd
 
-
-
-
-
-
+if "%selection%" == "quit" goto quit
 goto int
 pause
+
+:newgame
+cls
+echo Creating variables... (0/4)
+
+echo set x=1 > savegame.cmd
+echo Creating variables... (1/4)
+
+echo set y=1 >> savegame.cmd
+echo Creating variables... (2/4)
+
+echo set playername=Batch >> savegame.cmd
+echo Creating variables... (3/4)
+
+echo call start.bat >> savegame.cmd
+echo Finishing savegame... (4/4)
+sleep 1
+call savegame.cmd
+goto int
+pause
+:quit
--- start.bat
+++ start.bat
@@ -2,21 +2,22 @@
 rem This is the part of the loop chosing what part of the map you are in. Add more maps with the pattern described. (everything is copy/paste)
 rem If you make a tile with custom activities like dungeons that may require variables, please add resets either to the home.bat file or just below this comment.
 
-
-if %x%==1 if %y%==1 call 001001
-if %x%==1 if %y%==2 call 001002
-if %x%==1 if %y%==3 call 001003
-if %x%==1 if %y%==4 call 001004
-if %x%==1 if %y%==5 call 001005
-if %x%==1 if %y%==6 call 001006
-if %x%==1 if %y%==7 call 001007
-if %x%==1 if %y%==8 call 001008
-if %x%==1 if %y%==9 call 001009
-if %x%==2 if %y%==1 call 002001
-if %x%==2 if %y%==2 call 002002
+if %x%==1 if %y%==1 call %cd%/bin/001001.bat
+if %x%==1 if %y%==2 call %cd%/bin/001002.bat
+if %x%==1 if %y%==3 call %cd%/bin/001003.bat
+if %x%==1 if %y%==4 call %cd%/bin/001004.bat
+if %x%==1 if %y%==5 call %cd%/bin/001005.bat
+if %x%==1 if %y%==6 call %cd%/bin/001006.bat
+if %x%==1 if %y%==7 call %cd%/bin/001007.bat
+if %x%==1 if %y%==8 call %cd%/bin/001008.bat
+if %x%==1 if %y%==9 call %cd%/bin/001009.bat
+if %x%==2 if %y%==1 call %cd%/bin/002001.bat
+if %x%==2 if %y%==2 call %cd%/bin/002002.bat
 
 rem call game loops to the top of the page, repeating the level creation and everything else.
-if "%selection%" == "quit" goto quit else goto game
+
+if "%selection%" == "quit" goto quit
+goto game
 
 
 
